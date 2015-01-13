dd31ce17e67242942830382b69f02bce0a144ca7
Jorge García-Luengo <jorgegarcialuengo@gmail.com>
2015-01-13 12:09:35 -0400

    couting words

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,11 +1,33 @@
 module FunWithStrings
   def palindrome?
     # your code here
-	$local_string = self.downcase.gsub(/\W/, '')
-	$local_string == $local_string.reverse ? true : false
+	@local_string = self.downcase.gsub(/\W/, '')
+	@local_string == @local_string.reverse ? true : false
   end
   def count_words
     # your code here
+	@local_string = self.downcase
+	@local_array = @local_string.split
+	@local_array.each {|n| n.gsub!(/\W/, '')}
+	@local_array.delete_if {|x| x.length == 0}
+	@local_hash = Hash.new
+
+	@local_unique_array = @local_array.uniq
+	
+	@local_unique_array.each {|n|
+
+			number_of_words = 0
+			@local_array.each {|m| 	
+				if n == m			
+					number_of_words += 1
+				end
+			}  
+
+			@local_hash[n] = number_of_words
+		}
+
+	return @local_hash
+
   end
   def anagram_groups 
     # your code here
