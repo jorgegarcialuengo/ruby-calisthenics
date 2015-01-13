93ca0352f5037bf7d63453a7ffbeddc6f97bdd71
Jorge García-Luengo <jorgegarcialuengo@gmail.com>
2015-01-13 10:23:15 -0400

    fixed para..

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,11 +1,13 @@
 module FunWithStrings
   def palindrome?
     # your code here
+	$local_string = self.downcase.gsub(/\W/, '')
+	$local_string == $local_string.reverse ? true : false
   end
   def count_words
     # your code here
   end
-  def anagram_groups
+  def anagram_groups 
     # your code here
   end
 end
